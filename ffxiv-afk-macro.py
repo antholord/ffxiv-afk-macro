8fff8d24611375c2610f257f6ee53ab48ede8a48
Anthony Lord <antholord@hotmail.com>
2021-12-11 16:52:07 -0500

    add prints

--- ffxiv-afk-macro.py
+++ ffxiv-afk-macro.py
@@ -3,8 +3,9 @@ import random
 import time
 from pywinauto import Application
 app = Application().connect(title="FINAL FANTASY XIV", visible_only=False, top_level_only=False, enabled_only=False)
-
+print("FFXIV AFK MACRO :: CONNECTED")
 while True:
+      print("FFXIV AFK MACRO :: WAKING UP")
       app.top_window().set_focus()
       keyboard.press_and_release('d')
       time.sleep(random.randint(1, 3))
